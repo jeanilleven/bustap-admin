a1a78fb179c15d32f02be35bf4d52b35fd31b136
Jeanille <jeanillevenabayon30@gmail.com>
2020-11-19 15:59:57 +0800

    added profit indicator

---
+++ lib/partials/profitindicator.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+
+class ProfitIndicator extends StatefulWidget {
+  ProfitIndicator({Key key}) : super(key: key);
+
+  @override
+  _ProfitIndicatorState createState() => _ProfitIndicatorState();
+}
+
+class _ProfitIndicatorState extends State<ProfitIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 200,
+        width: 200,
+        child: LiquidCircularProgressIndicator(
+          value: 0.5, // Defaults to 0.5.
+          valueColor: AlwaysStoppedAnimation(Colors.cyan[100]), // Defaults to the current Theme's accentColor.
+          backgroundColor:Colors.white, // Defaults to the current Theme's backgroundColor.
+          borderColor: Colors.teal,
+          borderWidth: 2.0,
+          direction: Axis.vertical, // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
+          center: Text("60%",
+              style: TextStyle(
+                fontSize: 40, 
+                color: Colors.teal),
+        )));
+  }
+}
--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../partials/maindrawer.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:percent_indicator/percent_indicator.dart';  
+import '../partials/profitindicator.dart';
 
 class Dashboard extends StatelessWidget {
   @override
@@ -50,107 +50,90 @@ class DashboardGrid extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: EdgeInsets.all(20),
-            child: Container(
-              child: type
-            ),
+            child: Container(child: type),
           ),
         ));
   }
 
   Column vehicle(String title, Color textcolor) {
     return Column(
-                children: <Widget>[
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(title,
-                            style: TextStyle(color: textcolor, fontSize: 20)),
-                      ]),
-                  Divider(
-                    height: 50,
-                    endIndent: 400,
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Text("300",
-                            style: TextStyle(
-                                color: textcolor,
-                                fontSize: 50,
-                                fontWeight: FontWeight.w600)),
-                        Text(" out of ",
-                            style: TextStyle(
-                                color: textcolor,
-                                fontSize: 30,
-                                fontWeight: FontWeight.w100)),
-                        Text("500",
-                            style: TextStyle(
-                                color: textcolor,
-                                fontSize: 50,
-                                fontWeight: FontWeight.w600))
-                      ])
-                ],
-              );
+      children: <Widget>[
+        Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(title, style: TextStyle(color: textcolor, fontSize: 20)),
+            ]),
+        Divider(
+          height: 50,
+          endIndent: 400,
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text("300",
+                  style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600)),
+              Text(" out of ",
+                  style: TextStyle(
+                      color: textcolor,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w100)),
+              Text("500",
+                  style: TextStyle(
+                      color: textcolor,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600))
+            ])
+      ],
+    );
   }
 
-  Column profit(String title, Color textcolor){
+  Column profit(String title, Color textcolor) {
     return Column(
-                children: <Widget>[
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(title,
-                            style: TextStyle(color: textcolor, fontSize: 20)),
-                      ]),
-                  Divider(
-                    height: 50,
-                    endIndent: 400,
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Text("100,000.00",
-                            style: TextStyle(
-                                color: textcolor,
-                                fontSize: 50,
-                                fontWeight: FontWeight.w600)
-                        ),
-                  ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding( 
-                        padding: EdgeInsets.all(20),
-                        child: CircularPercentIndicator(
-                          progressColor: Colors.amber[600],
-                          radius: 200.0,
-                          lineWidth: 5.0,
-                          percent: 1.0,
-                          center: new Text("60%", style: TextStyle(color: Colors.green, fontSize: 30)),
-                        )
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text("higher than yesterday's ratings",
-                          style: TextStyle(
-                            color: textcolor,
-                            fontSize: 18
-                          )
-                      )
-                    ],
-                    
-                  )
-                ],
-              );
+      children: <Widget>[
+        Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(title, style: TextStyle(color: textcolor, fontSize: 20)),
+            ]),
+        Divider(
+          height: 50,
+          endIndent: 400,
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text("100,000.00",
+                  style: TextStyle(
+                      color: textcolor,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600)),
+            ]),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: ProfitIndicator())
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text("higher than yesterday's ratings",
+                style: TextStyle(color: textcolor, fontSize: 18))
+          ],
+        )
+      ],
+    );
   }
 
   @override
@@ -163,11 +146,11 @@ class DashboardGrid extends StatelessWidget {
       children: <Widget>[
         dashboardItem(vehicle("Buses Running", Colors.blueGrey[900])),
         dashboardItem(profit("Today's Earnings", Colors.blueGrey[900])),
-        dashboardItem(vehicle("Jeep Running", Colors.blueGrey[900] )),
+        dashboardItem(vehicle("Jeep Running", Colors.blueGrey[900])),
       ],
       staggeredTiles: [
         StaggeredTile.extent(2, 250),
-        StaggeredTile.extent(2, 515),
+        StaggeredTile.extent(2, 526),
         StaggeredTile.extent(2, 250),
       ],
     );
