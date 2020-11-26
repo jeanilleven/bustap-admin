4c07841fd063eb84fc8f63264bd8ab55f4b65972
Jeanille <jeanillevenabayon30@gmail.com>
2020-11-26 23:58:22 +0800

    modified the calendar

--- lib/partials/schedule.dart
+++ lib/partials/schedule.dart
@@ -35,30 +35,45 @@ class _ScheduleTableState extends State<ScheduleTable> {
                         builder: (BuildContext context) => new Vehicles()));
               },
             )),
-        body: SingleChildScrollView(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-                padding: EdgeInsets.fromLTRB(100, 30, 100, 100),
-                child: TableCalendar(
-                  initialCalendarFormat: CalendarFormat.twoWeeks,
-                  calendarController: _controller,
-                  calendarStyle: CalendarStyle(
-                      todayColor: Colors.blue[100],
-                      todayStyle:
-                          TextStyle(color: Colors.white,fontSize: 20, fontWeight: FontWeight.bold),
-                      selectedColor: Colors.cyan[400],
-                      selectedStyle:
-                          TextStyle(color: Colors.black, fontSize: 20)),
-                  headerStyle: HeaderStyle(
-                    formatButtonShowsNext: false,
-                  ),
-                  onDaySelected: (day, events, holidays) {
-                    print(day.toString());
-                  },
-                ))
-          ],
-        )));
+        body: Padding(
+          padding: EdgeInsets.all(30),
+          child: Material(
+            elevation: 20, 
+            borderRadius: BorderRadius.circular(20),
+            shadowColor: Colors.blueGrey,
+            child: SingleChildScrollView(
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                    child: TableCalendar(
+                      initialCalendarFormat: CalendarFormat.twoWeeks,
+                      calendarController: _controller,
+                      calendarStyle: CalendarStyle(
+                          todayColor: Colors.blue[100],
+                          todayStyle:
+                              TextStyle(color: Colors.white,fontSize: 20, fontWeight: FontWeight.bold),
+                          selectedColor: Colors.cyan[400],
+                          selectedStyle:
+                              TextStyle(color: Colors.black, fontSize: 20)),
+                      headerStyle: HeaderStyle(
+                        formatButtonShowsNext: false,
+                      ),
+                      onDaySelected: (day, events, holidays) {
+                        print(day.toString());
+                      },
+                    ))
+              ],
+            ))
+          ) 
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          tooltip: 'Assign a new schedule',
+          child: Icon(CupertinoIcons.add)
+        ),
+        
+        );
   }
 }
